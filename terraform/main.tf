92de106ad0b77e9be090ed59f855beb4192d3031
Wilmer Mendez <wilmer.mendez@globant.com>
2020-07-10 09:31:40 -0500

    Fix terraform version

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,11 +1,13 @@
-provider "aws" {
-  region = var.region
-}
-
 terraform {
+  required_version = "0.12.24"
+
   backend "s3" {
     bucket = "tech-insiders-tdi"
     key    = "terraform.tfstate"
     region = "us-east-1"
   }
+}
+
+provider "aws" {
+  region = var.region
 }
