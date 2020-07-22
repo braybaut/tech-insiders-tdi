59bcf925af2ae8556fffd154dc8fa142b7d865a9
Wilmer Mendez <wilmer.mendez@globant.com>
2020-07-21 19:43:24 -0500

    Fix http body error

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 )
 
 func homePage(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Welcome to fail test")
+	fmt.Fprintf(w, "Welcome to tech insiders")
 	fmt.Println("Endpoint Hit: homePage")
 }
 
